6e6f4c6284dbbec3c2eab8fe16a4047b531d790b
prozombie <prozombie@prozombie.local>
2017-07-19 22:19:20 +0800

    first commit

---
+++ sell/src/components/star/star.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="star" :class="starType">
+      <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+      props: {
+          size: {
+              type: Number
+          },
+          score: {
+              type: Number
+          }
+      }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
